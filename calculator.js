293c164e73ced4aaad5438da5b689d3225b69123
Viktor741 <43399818+Viktor741@users.noreply.github.com>
2018-10-31 11:12:14 +0100

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,29 @@
+const calculator = {
+  displayValue: "0",
+  FirstOperand: false,
+  operator: null
+};
+function updateDisplay() {
+  const display = document.querySelector(".calculator-screen");
+  display.value = calculator.displayValue;
+}
+updateDisplay();
+function inputDigit(digit) {
+  if (calculator.displayValue == "0") calculator.displayValue = digit;
+  else calculator.displayValue = calculator.displayValue + digit;
+}
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", event => {
+  const { target } = event;
+  if (!target.matches("button")) {
+    return;
+  }
+  if (target.classList.contains("operator")) {
+    return;
+  }
+  if (target.classList.contains("all-clear")) {
+    return;
+  }
+  inputDigit(target.value);
+  updateDisplay();
+});
